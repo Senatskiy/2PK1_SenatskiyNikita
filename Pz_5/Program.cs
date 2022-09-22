0b795697f01fa4179419cd4bcc6476c1046ef199
Никита Сенацкий <nik_senatskiy@mail.ru>
2022-09-22 20:01:10 +0500

    19 вариант

--- Pz_5/Program.cs
+++ Pz_5/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 10;
+            int b = 60;
+            for (; a <= b; a+=3)
+            {
+                Console.WriteLine(a.ToString());
+            }
+            Console.WriteLine("Первое задание выполнено");
+            {
+                char f = 'Д';
+                for (int i = 0; i <= 6; i++)
+                {
+                    Console.WriteLine(f);
+                    f++;
+                }
+                Console.WriteLine("Второе задание выполнено");
+            }
+            for (var i = 0; i <= 8; i++)
+            {
+                for (var j = 0; j < 5; j++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Третье задание выполнено");
+            {
+                for (int i = 0; i <= 100; i++)
+                    if (i % 3.5 == 0) Console.WriteLine(i);
+            }
+            Console.WriteLine("Четвертое задание выполнено");
+            {
+                for (int i = 5, j = 82; i - j != 35; i++, j--)
+                {
+                    Console.WriteLine(i + "  " + j);
+                }
+                Console.WriteLine("Пятое задание выполнено");
+
+
+            }
         }
     }
 }
