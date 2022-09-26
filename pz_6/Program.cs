91b362426fc20b0bb472ab4b7d139fc2ce0b3a87
Никита Сенацкий <nik_senatskiy@mail.ru>
2022-09-26 19:24:53 +0500

    вариант 19

---
+++ pz_6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1;
+            while (x <= 3)
+            {
+                double y = 8 * x + 4 * Math.Pow(x, 4) - 1.5 * Math.Pow(x, 3);
+                x += 0.2;
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
