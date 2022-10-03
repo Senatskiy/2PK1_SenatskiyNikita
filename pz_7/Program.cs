b3b232ec022128f92585566e1f1d0fab74be0217
Никита Сенацкий <nik_senatskiy@mail.ru>
2022-10-03 20:57:48 +0500

    Вариант 19

---
+++ pz_7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Senatskiy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massiv = { -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int min = massiv.Min();
+            int max = massiv.Max();
+            int count = massiv.Count(numb => max < -numb);
+            if (max >= -min)
+                Console.WriteLine($"В массиве минимальным по модулю элементом является число {count}.");
+
+        }
+    }
+}
