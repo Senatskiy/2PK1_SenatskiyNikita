80943593ce12755492f68182a46f72d2d15a0d7b
Senatskiy <114167376+Senatskiy@users.noreply.github.com>
2022-10-05 20:17:17 +0500

    Add files via upload

---
+++ pz_8/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace pz_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int n = 5, mult = 1;
+            int[,] a = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = rand.Next(0, 10);
+                    Console.Write(string.Format("{0,2} ", a[i, j]));
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < n; i++)
+                if (a[i, i] > 0)
+                    mult *= a[i, i];
+
+            Console.Write("произведение = {0}", mult);
+            Console.ReadLine();
+
+        }
+    }
+}\
