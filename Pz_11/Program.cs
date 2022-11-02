1e6e05f83885d2a6bad9785d17008e2581ee918c
Senatskiy <114167376+Senatskiy@users.noreply.github.com>
2022-11-02 20:51:55 +0500

    Add files via upload

---
+++ Pz_11/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace pz_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string box = "";
+            string text = Console.ReadLine();
+            while (text != "exit")
+            {
+                string result;
+                result = box + text;
+                box = result;
+                Console.WriteLine(result);
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
