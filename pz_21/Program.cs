eee50f2c2d049b00be47d3f9b864e0328cd3e911
Senatskiy <114167376+Senatskiy@users.noreply.github.com>
2023-05-06 18:57:55 +0500

    19 вариант

---
+++ pz_21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(5, 7);
+            rectangle.PrintInfo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pz_21/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_21
+{
+    internal class Rectangle
+    {
+        private double a;
+        private double b;
+
+        public Rectangle(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public double GetVolume()
+        {
+            return 0;
+
+        }
+
+        public double GetArea()
+        {
+            return a * b;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Прямоугольник со сторонами {a} и {b}, площадью {GetArea()}");
+        }
+    }
+}
