df8484e3db38794191e49cb9cb1ad380f52e9da9
Senatskiy <114167376+Senatskiy@users.noreply.github.com>
2023-06-13 15:51:56 +0500

    Программа для отчета

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace wrx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] Shifer = { 'Ф', 'А', 'М', 'И', 'Л', 'И', 'Я' };
+            char[] result = new char[Shifer.Length];
+            Console.Write("TEXT: ");
+            Console.WriteLine(Shifer);
+
+            for (int i = 0; i < Shifer.Length; i++)
+            {
+                result[i] = (char)(Shifer[i] + '5');
+            }
+            Console.Write("SHIFR + 5: ");
+            Console.WriteLine(result);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = (char)(result[i] - 5);
+            }
+            Console.Write("DESHIFR - 5");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
